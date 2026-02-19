6d9eae0a029b0aa00f40a1e5fbd8f2a47f37b3c0
Nurdaulet <b.nurdauet2626@gmail.com>
2026-02-19 20:11:52 +0500

    Docker was implemented

--- internal/adapters/postgres/database.py
+++ internal/adapters/postgres/database.py
@@ -1,9 +1,14 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://todo_user:password@localhost:5432/todo_db" #"postgresql://ml_user:ml_pass@localhost:5432/ml_experiments"
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql+psycopg2://ml_user:ml_pass@localhost:5432/ml_experiments",
+)
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 def get_db_session():
